12181e35be7934566b5d9d993af7d725fb934614
edmundas04 <edmundas04@gmail.com>
2016-10-16 19:07:41 +0300

    Added validations when creating timetable

--- src/TimetableSolver/Models/Timetable.cs
+++ src/TimetableSolver/Models/Timetable.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace TimetableSolver.Models
@@ -19,10 +20,65 @@ namespace TimetableSolver.Models
             List<Contracts.TeacherAssignedTeachingGroup> teacherAssignedTeachingGroups,
             List<Contracts.AvailableWeekDay> availableWeekDays)
         {
+            Check(classes, teachers, teachingGroups, classAssignedTeachingGroups, teacherAssignedTeachingGroups, availableWeekDays);
             TeachingGroups = TransformTeachingGroups(teachingGroups);
             Teachers = TransformTeachers(teachers, teacherAssignedTeachingGroups);
             Classes = TransformClasses(classes, classAssignedTeachingGroups);
             AvailableWeekDays = TransformAvailableWeekDays(availableWeekDays);
+        }
+
+        private void Check(List<Contracts.Class> classes,
+            List<Contracts.Teacher> teachers, List<Contracts.TeachingGroup> teachingGroups, 
+            List<Contracts.ClassAssignedTeachingGroup> classAssignedTeachingGroups,
+            List<Contracts.TeacherAssignedTeachingGroup> teacherAssignedTeachingGroups,
+            List<Contracts.AvailableWeekDay> availableWeekDays)
+        {
+            if (!classAssignedTeachingGroups.All(x => classes.Any(c => c.Id == x.IdClass)))
+            {
+                throw new ArgumentException("Some classAssignedTeachingGroups has no reference to class");
+            }
+
+            if (!classAssignedTeachingGroups.All(x => teachingGroups.Any(c => c.Id == x.IdTeachingGroup)))
+            {
+                throw new ArgumentException("Some classAssignedTeachingGroups has no reference to teaching group");
+            }
+
+            if (!teacherAssignedTeachingGroups.All(x => teachers.Any(c => c.Id == x.IdTeacher)))
+            {
+                throw new ArgumentException("Some teacherAssignedTeachingGroups has no reference to teacher");
+            }
+
+            if (!teacherAssignedTeachingGroups.All(x => teachingGroups.Any(c => c.Id == x.IdTeachingGroup)))
+            {
+                throw new ArgumentException("Some teacherAssignedTeachingGroups has no reference to teaching group");
+            }
+
+            if(!classes.All(c => classAssignedTeachingGroups.Any(a => a.IdClass == c.Id)))
+            {
+                throw new ArgumentException("Some classes has no reference to teaching group");
+            }
+
+            if (!teachers.All(t => teacherAssignedTeachingGroups.Any(a => a.IdTeacher == t.Id)))
+            {
+                throw new ArgumentException("Some classes has no reference to teaching group");
+            }
+
+            if (availableWeekDays.Count != availableWeekDays.Select(s => s.DayOfWeek).Distinct().Count())
+            {
+                throw new ArgumentException("Some availableWeekDays duplicated");
+            }
+
+            if(!availableWeekDays.All(x => x.NumberOfLessons > 0))
+            {
+                throw new ArgumentException("NumberOfLessons must be greater than 0");
+            }
+
+            var allTimetables = teachingGroups.SelectMany(s => TransformTimetableElements(s.Timetable)).Distinct();
+            var allowedValues = TimetableHelper.AvailableTimes(TransformAvailableWeekDays(availableWeekDays));
+            if (!allTimetables.All(x => allowedValues.Contains(x)))
+            {
+                throw new ArgumentException("Some timetable values are incorrect");
+            }
         }
 
         private List<KeyValuePair<short, short>> TransformAvailableWeekDays(List<Contracts.AvailableWeekDay> availableWeekDays)
